a721c560755d00ed58d2a5e3fa3ecc10af5df9bf
Lordera1209 <songzy1209@gmail.com>
2024-07-23 13:40:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
 # 设置页面标题
-st.title('Simple Calculator')
+st.title('简易计算器')
 
 # 添加文本输入框用于输入数字
-num1 = st.number_input('Enter the first number:')
-num2 = st.number_input('Enter the second number:')
+num1 = st.number_input('请输入第一个数字:')
+num2 = st.number_input('请输入第二个数字:')
 
 # 添加下拉框选择运算符
-operator = st.selectbox('Select an operator:', ['+', '-', '*', '/'])
+operator = st.selectbox('选择运算符:', ['+', '-', '*', '/'])
 
 # 定义计算逻辑
-if st.button('Calculate'):
+if st.button('计算'):
     if operator == '+':
         result = num1 + num2
     elif operator == '-':
@@ -22,5 +22,5 @@ if st.button('Calculate'):
         if num2 != 0:
             result = num1 / num2
         else:
-            result = 'Division by zero is undefined'
+            result = '除数不能为0！'
     st.write('Result:', result)
